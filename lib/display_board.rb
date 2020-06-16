d9a6f309772179c9b543a4f9bbe2e6d013b86e1b
Wilson Ng <wilsongch92@gmail.com>
2020-06-16 12:46:19 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,9 @@
 board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
 
 def display_board(board)
-  puts " #{board[0]} |   |   " 
+  puts " #{board[4]} |  #{board[4]} |  #{board[4]} " 
   puts "-----------"
-  puts "   | #{board[4]} |   "
+  puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 end
