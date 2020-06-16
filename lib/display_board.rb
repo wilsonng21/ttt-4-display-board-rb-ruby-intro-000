0b8c422f01e7d255b635e7d7cf2d572e0e1f8d6d
Wilson Ng <wilsongch92@gmail.com>
2020-06-16 12:36:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
-blank_board = ["   |   |   ", "-----------", "   |   |   ", "-----------", "   |   |   "]
+
 
 def display_board(board)
-  puts blank_board
+  puts "   |   |   " 
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
 
 display_board(board)
